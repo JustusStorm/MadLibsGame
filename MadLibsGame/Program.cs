f8c6aac62e6c317fdbbbc31b965abccb8763d550
Justus Storm <storm.j.dev@gmail.com>
2020-08-24 23:48:09 -0500

    Finished 1st draft of the game

--- MadLibsGame/Program.cs
+++ MadLibsGame/Program.cs
@@ -1,110 +1,67 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using Custom;
 
 namespace MadLibsGame
 {
     class Program
     {
-
-        // creates a little banner to use to seperate chunks of information
-        static void Banner()
-        {
-            Console.WriteLine("**********************************************************************************");
-        }
-
-        // use this to print a statement to the user telling them their input is invalid for a yes or no question
-        static void InvalidInputYesNo()
-        {
-            string input;
-            do
-            {
-                Console.WriteLine("\n Your input is an invalid answer. Please type yes or no in the input.\n");
-                Console.Write("> ");
-                input = Console.ReadLine().ToLower().Trim();
-            } while (input != "yes" || input != "no") ;
-        }
-
-
-
-        // function that allows you to take input from the user
-        static void GetInput(out string input)
-        {
-            Console.Write("> ");
-            input = Console.ReadLine().ToLower().Trim();
-
-        }
-
-
-
-        // Function to start the game, making sure user knows how game is played and explains how to play
-        static void StartGame()
-        {
-            string y = "yes";
-            string n = "no";
-            string input;
-
-
-            Console.WriteLine("Welcome to my Mad Libs game! Do you know how Mad Libs work? (yes/no) \n");
-            GetInput(out input);
-
-            if (input == "yes")
-            {
-                Console.WriteLine("\nThen lets get started!\n");
-            }
-            else if (input == "no")
-            {
-                Console.WriteLine("\n Okay, so I'm going to ask you to give me some information like nouns, adverbs, actions, etc. \n " +
-                    "I'm going to use the answers you give me to place them in a funny sentence! Best part about it, you don't know \n" +
-                    "what the sentence is going to be before you give me the answers.\n");
-
-                Console.WriteLine("\n Do you understand how to play? (yes/no) \n");
-                GetInput(out input);
-
-                if (input == "yes")
-                {
-                    Console.WriteLine("\n Lets play!\n");
-                }
-                else
-                {
-                    Console.WriteLine("\n Go to Google and look up MadLibs for more information on the game style.\n");
-                }
-            }
-            else
-            {
-                InvalidInputYesNo();
-
-            }
-        }
-            
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-        // Start of Program
-
         static void Main(string[] args)
         {
-            Banner();
+            Console.WriteLine("Lets play Mad Libs!");
+            UI.Spacer();
+            string name = AskName();
+            string noun = AskNoun();
+            string verb = AskVerb();
+            string place = AskPlace();
+            UI.Spacer();
 
-            StartGame();
-            Thread.Sleep(2000); // allows 2 seconds between last input in StartGame() and clearing the terminal
-            Console.Clear();
+            string[] sentences =
+            {
+                $"Did you hear about what {name} did near the {place}? They said he {verb} his {noun} a little too hard last night.",
+                $"I wonder how {name} feels about how Chad {verb} his {noun} behind the {place}.", 
+                $"You see that {place}, thats where {name} {verb} with a {noun}a few days ago.",
+                $"I told {name} to move thier {noun}, but they didn't, so I {verb} their {noun} all the way to the {place}", 
+                $"I rode that {noun} so hard after {name} {verb} it at the {place}.",
+                $"{name} said they {verb} {noun} at the {place}, and I got a video of it.",
+                $"{name}'s {noun} fell out of their pocket in the middle of the {place}. I was really {verb}.",
+                $"{name} painted a {noun} on the front of Jimmy's {place}. When he saw it he {verb} {name} worse than a fat kid eating cake. ",
+                $"When we got closer to {name}'s {place}, Peyton {verb} thier {noun} like she had never seen it before.",
+                $"{name} {verb} their little {noun} a little more when we arrived at {name}'s {place}."
 
-            // Console.WriteLine("Test");
+            };
+
+            UI.Spacer();
+            Random rand = new Random();
+            int index = rand.Next(0, sentences.Length);
+            Console.WriteLine(sentences[index]);
+
+
         }
+        static string AskName()
+        {
+            Console.Write("Enter a name:");
+            return Console.ReadLine().Trim();
+        }
+        static string AskNoun()
+        {
+            Console.Write("Enter a noun(thing, animal, object, etc.):");
+            return Console.ReadLine().Trim();
+        }
+        static string AskVerb()
+        {
+            Console.Write("Enter a action (verb, past tense):");
+            return Console.ReadLine().Trim();
+        }
+        static string AskPlace()
+        {
+            Console.Write("Enter a place:");
+            return Console.ReadLine().Trim();
+        }
+        
+        
+
     }
 }
     
